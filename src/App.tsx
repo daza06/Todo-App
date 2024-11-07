dd4d6bfcf05869dcef026f970a2c04f030938c78
Daniel Zavala <link123852@gmail.com>
2024-11-06 22:47:28 -0700

    Created first component (InputTask)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import InputTask from "./components/InputTask";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <span className="heading">My Tasks</span>
+      <InputTask />
     </div>
   );
 };
---
+++ src/components/InputTask.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InputTask = () => {
+  return <div>InputTask</div>;
+};
+
+export default InputTask;
