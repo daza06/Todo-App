4913b0a2deeeb65bb56b0df87b800d4ea41096e6
Daniel Zavala <link123852@gmail.com>
2024-11-09 21:44:44 -0700

    Updated InputTask to add Typescript logic for entering tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
-import InputTask from "./components/InputTask";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <span className="heading">My Tasks</span>
-      <InputTask />
     </div>
   );
 };
--- src/components/InputTask.tsx
+++ src/components/InputTask.tsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-const InputTask = () => {
+export interface Task {
+  id: number;
+  name: string;
+  description: string;
+}
+interface InputTaskProps {
+  onAddTask: (task: { name: string; description: string }) => void;
+}
+
+const InputTask: React.FC<InputTaskProps> = ({ onAddTask }) => {
+  const [name, setName] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+
+  // handle submission
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault(); // prevent page reload
+
+    // call onAddTask function passed via props
+    if (name && description) {
+      onAddTask({ name, description });
+      setName(""); // clear name input after submission
+      setDescription(""); // clear description input after submission
+    }
+  };
+
   return (
-    <form className="input">
-      <input type="input" placeholder="Enter a task" />
-      <button type="submit">+</button>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter a task"
+      />
+      <textarea
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="Task description"
+      />
+      <button className="submit" type="submit">
+        +
+      </button>
     </form>
   );
 };
